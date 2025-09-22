c52f3ff7fc9cb81afd72f4aabbe99b1b03f2a1de
asfandkhan8585-cpu <asfandkhan8585@gmail.com>
2025-09-22 19:17:36 +0500

    Added Asfand folder with Python files

---
+++ Asfand/operations.py
@@ -0,0 +1,21 @@
+# arithmetic operators
+
+a = 2
+b = 10
+
+
+print ("addition =",a+b)
+print ("multiplication =",a*b)
+print ("subtraction=",a-b)
+
+#relational operators
+print(a==10)
+print(a!=b)
+
+#logical operators
+print("Multiplication:", a * b)
+print("Division :", a / b)
+print("Floor Division :", a // b)
+print("Modulus:", a % b)
+print("Exponent :", a ** b)
+
